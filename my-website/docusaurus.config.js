c4bdda9e5efcf452f8d5da7bb736bea00a885b71
Juancho <juanjqogm@gmail.com>
2022-08-17 13:55:10 +0900

    Fixed the config file

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -17,8 +17,8 @@ const config = {
   // GitHub pages deployment config. 
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'juanjqo', // Usually your GitHub org/user name.
-  projectName: 'juanjqo', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  projectName: 'juanjqo.github.io', // Usually your repo name.
+  deploymentBranch: 'main',
   trailingSlash: false,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
