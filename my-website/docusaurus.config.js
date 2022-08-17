f3d08dbddb6c8597a329b41ab3bbb1a466fb1e56
Juancho <juanjqogm@gmail.com>
2022-08-17 15:44:52 +0900

    Updated the config file to use netlify

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Juan José Quiroz Omaña, Ph.D.',
   tagline: '',
   url: 'https://juanjqo.github.io',  // 'https://docusaurus-2.netlify.app'  'https://juanjqo.github.io'
-  baseUrl: '/juanjqo.github.io/',
+  baseUrl: '/', // '/juanjqo.github.io/'
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/manipulator.png', 
