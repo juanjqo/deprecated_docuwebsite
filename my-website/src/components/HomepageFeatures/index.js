2f07d70c2b7178fd672b87a6c0ff9df228c141a5
juancho <juanjqogm@gmail.com>
2022-10-21 23:51:35 +0900

    Updated the profile photo.

--- my-website/src/components/HomepageFeatures/index.js
+++ my-website/src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Juan J. Quiroz O.',
-    Svg: require('@site/static/img/photo_2.svg').default,
+    Svg: require('@site/static/img/photo_3.svg').default,
     description: (
       <>
         I'm a robotics researcher at The University of Tokyo.
